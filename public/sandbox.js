06b8822714101716cf6c2bf9b0697701b0fb2bd6
anastasiya025 <anastasiya025.am@gmail.com>
2021-05-22 14:50:44 +0300

    refactor: create Function Basics (void)

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,6 @@
 "use strict";
 const inputs = document.querySelectorAll("input");
-console.log(inputs);
+//console.log(inputs);
 inputs.forEach(input => {
     console.log(input);
 });
@@ -10,11 +10,11 @@ let isBlackBelt = false;
 const calc = (num) => {
     return num * Math.PI;
 };
-console.log(calc(6));
+//console.log(calc(6));
 function writter(str) {
     console.log(str.split(''));
 }
-writter("5678");
+//writter("5678");
 let nastja = {
     name: "Asja",
     age: 40,
@@ -23,4 +23,15 @@ let nastja = {
 console.log(nastja);
 nastja.age = 45;
 nastja.name = "Masha";
-console.log(nastja);
+//console.log(nastja);
+let great;
+great = (a, b) => {
+    return a * b;
+};
+let result = great(4, 7);
+console.log(result);
+const sum = (a, b, c = 45) => {
+    console.log(a + b);
+    console.log(c);
+};
+sum(3, 6, "str");
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,5 @@
 const inputs = document.querySelectorAll("input");
-console.log(inputs);
+//console.log(inputs);
 
 inputs.forEach(input => {
     console.log(input)
@@ -12,13 +12,13 @@ let isBlackBelt = false;
 const calc = (num:number) => {
     return num * Math.PI;
 }
-console.log(calc(6));
+//console.log(calc(6));
 
 function writter (str: string) {
     console.log (str.split(''));
 }
 
-writter("5678");
+//writter("5678");
 
 let nastja = {
     name: "Asja",
@@ -30,4 +30,19 @@ console.log(nastja);
 nastja.age = 45;
 nastja.name = "Masha";
 
-console.log(nastja);
+//console.log(nastja);
+
+let great: Function;
+
+great = (a: number, b: number): number => {
+    return a * b;
+}
+let result = great(4, 7);
+console.log(result);
+
+const sum = (a: number, b:number, c: number | string = 45): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+sum(3, 6, "str");
